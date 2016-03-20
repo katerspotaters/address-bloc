7b0f2e4b05eb4a369dec2a604d78a7ed9ae1ed8c
Katie <katelyn.hertel19@gmail.com>
2016-03-20 18:00:38 -0400

    Assignment 19 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -16,6 +16,19 @@ RSpec.describe AddressBook do
       book = AddressBook.new
       expect(book.entries.size).to eq(0)
     end
+
+    describe "#remove_entry" do
+      it "removes only one entry to the address book" do
+        book = AddressBook.new
+        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        book.add_entry('Katie Hertel', '551.427.4402', 'katelyn.hertel19@gmail.com')
+
+        expect(book.entries.size).to eq 2
+        book.remove_entry('Katie Hertel', '551.427.4402', 'katelyn.hertel19@gmail.com')
+        expect(book.entries.size).to eq 1
+    end
+  end
+
     describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
